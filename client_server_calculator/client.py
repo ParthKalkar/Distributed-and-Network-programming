8b721c15e83624a26d5860320fe5d3ea5212a870
Parth Kalkar <parthkalkar111@gmail.com>
2021-08-24 17:34:30 +0300

    week2

---
+++ client_server_calculator/client.py
@@ -0,0 +1,30 @@
+"""
+@author - Parth Kalkar
+To run - Download the file - open terminal in that folder -
+         type "python3 client.py ip_address port"
+
+**Make sure that the server.py is running before running this file**
+
+"""
+import socket
+import sys
+
+host = socket.gethostname()  # Get local machine name
+local_ip = socket.gethostbyname(host)
+local_port = int(sys.argv[2])
+
+serverAddressPort = (local_ip, local_port)
+bufferSize = 1024
+
+c = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+while True:
+    equ = input("Input regular expression for calculation: ")
+
+    sent_bytes = str.encode(str(equ))
+    c.sendto(sent_bytes, serverAddressPort)
+
+    server_message = c.recvfrom(bufferSize)
+    equ = format(server_message[0])
+
+    print(f"Result: {equ[2: -1]}\n")
